40b51f78b918ca387f19a972dee8b97b9b20c8e9
Fernanda Miranda <fernandamiranda2412@gmail.com>
2025-06-27 14:13:22 -0300

    Melhoria

--- src/Caneta.java
+++ src/Caneta.java
@@ -1,29 +1,42 @@
+//AULA 04- CURSO EM VÍDEO
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
+    public String modelo;
+    private float ponta;
+    private boolean tampada;
+    private String cor;
+    //Este é o metodo construtor
+    public Caneta(String m,String c, float p){
+        this.modelo=m;
+        this.tampar();
+        this.cor=c;
+        this.setPonta(p);// outra forma de construir
+        this.tampar();
 
-    void status(){
-        System.out.println("Modelo: " + this.modelo);
-        System.out.println("Cor: " + this.cor);
-        System.out.println("Ponta: " + this.ponta);
-        System.out.println("Carga: " + this.carga);
     }
-    void rabiscar(){
-        if(this.tampada==true){
-            System.out.println("ERRO! Não posso rabiscar!");
-        }else{
-            System.out.println("Estou rabiscando!");
-        }
+    public String getModelo(){
+        return this.modelo;
     }
+    public void setModelo(String m){
+        this.modelo=m;
+    }
+    public float getPonta(){
+        return this.ponta;
+    }
+    public void  setPonta(Float p){
+        this.ponta=p;
+    }
+    public void tampar(){
+        this.tampada=true;
+    }
+    public void destampar(){
+        this.tampada=false;
+    }
+    public void status(){
+        System.out.println("Sobre a Caneta");
+        System.out.println("Modelo: "+this.modelo);
+        System.out.println("Ponta"+this.ponta);
+        System.out.println("Cor: "+this.cor);
+        System.out.println("Tampada: "+this.tampada);
 
-    void tampar() {
-        this.tampada = true;
-    }
-
-    void destampar() {
-        this.tampada = false;
     }
 }
